8b695cfe2f4de0599430a05f716841f793491966
idodd <idoddii@gmail.com>
2022-11-17 20:46:38 +0200

    finished i hope

--- Generate_File.py
+++ Generate_File.py
@@ -1,8 +1,10 @@
 from main import find_rout
+from sys import argv
 import csv
 import ways
 
-ucsruns = open('results\\UCSRuns.txt', 'a+')
+mode = argv[1]
+file_export = open('results\\' + mode + 'Runs.txt', 'a+')
 roads = ways.load_map_from_csv()
 with open('ways\\problems.csv') as csv_file:
     csv_reader = csv.reader(csv_file, delimiter=',')
@@ -15,7 +17,10 @@ with open('ways\\problems.csv') as csv_file:
             break
         not_row = 0
         source, target = int(row[0]), int(row[1])
-        path, cost = find_rout(source, target, 'ucs', roads)
-        line = (' '.join(str(j) for j in path)) + ' - ' + str(cost) + '\n'
-        ucsruns.write(line)
-ucsruns.close()
+        path, cost, huristic_cost = find_rout(source, target, mode.lower(), roads)
+        line = (' '.join(str(j) for j in path)) + ' - ' + str(cost)
+        if huristic_cost != 0:
+            line += ' - ' + str(huristic_cost)
+        line += '\n'
+        file_export.write(line)
+file_export.close()
--- main.py
+++ main.py
@@ -3,59 +3,71 @@ Parse input and run appropriate code.
 Don't use this file for the actual work; only minimal code should be here.
 We just parse input and call methods from other modules.
 '''
-import ways
-
+import math
+from ways import tools, graph, info
+new_limit = 0
 
 # do NOT import ways. This should be done from other files
 # simply import your modules and call the appropriate functions
 
+
 def huristic_function(lat1, lon1, lat2, lon2):
-    distance = ways.tools.compute_distance(lat1, lon1, lat2, lon2)
+    distance = tools.compute_distance(lat1, lon1, lat2, lon2)
     return distance / 110
 
-
-def find_rout(source, target, mode, roads, limit):
+def find_rout(source, target, mode, roads):
     import heapq
+    from ways import info
     # loading the roads from the map.
     # creating the closed nodes list.
     closed = set()
     # creating the opened nodes list and adding the first node, where 0 is the cost to it, and -1 is the parent.
     opened = []
+    # Sets source node and target node to their respective junctions.
     source_node, target_node = roads[source], roads[target]
-    if mode == 'ucs':
-        opened.append((0, source_node, -1, 0))
-    else:
-        opened.append((huristic_function(source_node[1], source_node[2], target_node[1], target_node[2]),
-                       source_node, -1, 0))
+    huristic_cost = 0
+    # Sets the huristic cost from the source to target if the mode is astar
+    if mode == 'astar':
+        huristic_cost = huristic_function(source_node.lat, source_node.lon, target_node.lat, target_node.lon)
+    # Adds the source node to the open nodes list
+    opened.append((0, source_node, -1, 0))
     # creating a map of parents and putting source and target parents as -1, the algorithm will later change the target
     # parent.
     parents = {source: -1, target: -1}
-    cost, count = 0, 0
-    min_index = 0
+    cost = 0
     while opened:
+        # Takes the minimum out of the minimum heap
         next_node = heapq.heappop(opened)
+        # Ordering the tuple in variables for clarity
         prev_cost, parent, prev_huristics, next_node = next_node[0], next_node[2], next_node[3], next_node[1]
-        node_index = next_node[0]
-        if mode == 'idastar' and next_node[0] > limit:
-            count += 1
-            if count == 2:
-                return next_node[0]
+        node_index = next_node.index
+        # Sets the parent of the node in the parents map
         parents[node_index] = parent
+        # Adds the current node to the closed nodes list.
         closed.add(node_index)
-        links = next_node[3]
+        links = next_node.links
+        # If the current node is the target node it sets the cost for getting here and breaks out of the loop
         if target == node_index:
             cost = prev_cost
             break
+        # Looping over all the successors of the node
         for link in links:
-            if not link[1] in closed:
-                target_junc = roads[link[1]]
+            # Making sure the junction isn't in the closed junctions list.
+            if link.target not in closed:
+                target_junc = roads[link.target]
                 h, g = 0, prev_cost
+                # If we do ucs we don't need heuristic function, So it will be 0, if it's astar h will be the heuristic
+                # function
                 if mode == 'ucs':
                     h = 0
                 else:
-                    h = huristic_function(target_junc[1], target_junc[2], target_node[1], target_node[2])
+                    h = huristic_function(target_junc.lat, target_junc.lon, target_node.lat, target_node.lon)
+                # Sets f as the cost to the junction
                 f = g + h - prev_huristics
-                new_cost = link[2] + f
+                # Sets the new_cost as f + the distance to the link target divided by 1000 to convert it to km, and
+                # all this is divided by the max speed in the link
+                new_cost = f + ((link.distance / 1000) / info.SPEED_RANGES[link.highway_type][1])
+                # Checking if the link is in the open list.
                 found = False
                 index = 0
                 for item in opened:
@@ -63,6 +75,7 @@ def find_rout(source, target, mode, roads, limit):
                         found = True
                         break
                     index += 1
+                # If the link is in the open list, it updates its cost if new_cost is lower than his cost.
                 if found:
                     old_node = opened.pop(index)
                     if new_cost < old_node[0]:
@@ -70,41 +83,70 @@ def find_rout(source, target, mode, roads, limit):
                     heapq.heappush(opened, old_node)
                 else:
                     heapq.heappush(opened, (new_cost, target_junc, node_index, h))
-    if parents[target] == -1:
-        return min_index, cost
+    # Creates a list of the path from source to target and returns it with the cost and huristic function
     path = [target]
     parent = parents[target]
     while parent != -1:
         path.insert(0, parent)
         parent = parents[parent]
-    return path, cost
+    return path, cost, huristic_cost
 
 
 def find_ucs_rout(source, target):
-    roads = ways.load_map_from_csv()
-    path, cost = find_rout(source, target, 'ucs', roads, 0)
-    print(cost)
+    # Calling the findrout function with ucs mode and returns the path.
+    roads = graph.load_map_from_csv()
+    path, cost, x = find_rout(source, target, 'ucs', roads)
     return path
 
 
 def find_astar_route(source, target):
-    roads = ways.load_map_from_csv()
-    path, cost = find_rout(source, target, 'astar', roads, 0)
-    print(cost)
+    # Calling the findrout function with astar mode and returns the path.
+    roads = graph.load_map_from_csv()
+    path, cost, x = find_rout(source, target, 'astar', roads)
     return path
 
 
+def DFSF(state, g, path, f_limit, goal, roads):
+    global new_limit
+    # Sets the state and goal node from the roads.
+    state_node, goal_node = roads[state], roads[goal]
+    # Updating the new_f as the previous g + the heuristic function from this state to the goal
+    new_f = g + huristic_function(state_node.lat, state_node.lon, goal_node.lat, goal_node.lon)
+    # If the new_f is higher than f_limit it sets the new_limit to be the minimum of new_limit and new_f
+    if new_f > f_limit:
+        new_limit = min(new_limit, new_f)
+        return None
+    # If we reached the goal we return the path.
+    if state == goal:
+        return path
+    # Looping over all the successors.
+    for link in state_node.links:
+        # Activates the DFSF function on the next link.
+        path_copy = path.copy()
+        path_copy.append(link.target)
+        sol = DFSF(link.target, g + ((link.distance / 1000) / info.SPEED_RANGES[link.highway_type][1]), path_copy,
+                   f_limit, goal, roads)
+        if sol:
+            return sol
+    return None
+
+
 def find_idastar_route(source, target):
-    roads = ways.load_map_from_csv()
-    cost = 0
-    lim = huristic_function(roads[source][1], roads[source][2], roads[target][1], roads[target][2])
-    while cost == 0:
-        try:
-            path, cost = find_rout(source, target, 'idastar', roads, lim)
-            print(cost)
-            return path
-        except:
-            lim = find_rout(source, target, 'idastar', roads, lim)
+    roads = graph.load_map_from_csv()
+    global new_limit
+    # Sets the state and goal node from the roads.
+    source_node, target_node = roads[source], roads[target]
+    # Sets the new_limit to be the heuristic function from source to target goal.
+    new_limit = huristic_function(source_node.lat, source_node.lon, target_node.lat, target_node.lon)
+    # Keeps iterating until finding answer
+    while True:
+        # Sets the f_limit to be the new_limit and new_limit to be infinity
+        f_limit = new_limit
+        new_limit = math.inf
+        # Tries to find solution and if it succeeds, It returns the path.
+        sol = DFSF(source, 0, [source], f_limit, target, roads)
+        if sol:
+            return sol
 
 
 def dispatch(argv):
@@ -120,6 +162,5 @@ def dispatch(argv):
 
 
 if __name__ == '__main__':
-    x = [[0, 5], [1, 1], [2, 3], [1, 8], [4, 2], [3, 4]]
     from sys import argv
     dispatch(argv)
